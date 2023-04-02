96c19abc6d3392b8f3a6220c4044177c24df740c
markel1974 <markel@tin.it>
2023-04-02 08:39:28 +0200

    first

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/markel1974/webautoma/src/executor"
@@ -11,6 +13,7 @@ import (
 	"github.com/markel1974/webautoma/src/wd/caps/chrome"
 	"log"
 	"net/url"
+	"os"
 	"strings"
 )
 
@@ -19,60 +22,88 @@ import (
 //go mod vendor
 
 const (
-	defaultLogFile    = "log.json"
+	defaultResultFile = "log.json"
 	defaultImagesFile = "images.json"
 )
 
 func createCapture(capture string) []string {
-	var captureData []string
-	if len(capture) > 0 {
-		captureData = strings.Split(capture, ",")
+	if len(capture) == 0 {
+		return nil
 	}
-	return captureData
+	return strings.Split(capture, ",")
 }
 
 func createVariables(variables string) map[string]interface{} {
+	if len(variables) == 0 {
+		return nil
+	}
 	var varData map[string]interface{}
-	if len(variables) > 0 {
-		for _, v := range strings.Split(variables, ",") {
-			kv := strings.Split(v, "=")
-			if len(kv) < 2 {
-				continue
-			}
-			if varData == nil {
-				varData = make(map[string]interface{})
-			}
-			varData[strings.TrimSpace(kv[0])] = kv[1]
+	for _, v := range strings.Split(variables, ",") {
+		kv := strings.Split(v, "=")
+		if len(kv) < 2 {
+			continue
 		}
+		if varData == nil {
+			varData = make(map[string]interface{})
+		}
+		varData[strings.TrimSpace(kv[0])] = kv[1]
 	}
 	return varData
+}
+
+func launch(wdUrl *url.URL, sideFile string, resultFile string, imgFile string, capture string, variablesData map[string]interface{}) error {
+	if len(resultFile) == 0 {
+		resultFile = defaultResultFile
+	}
+
+	if len(imgFile) == 0 {
+		imgFile = defaultImagesFile
+	}
+	captureData := createCapture(capture)
+	exec, err := executor.New(sideFile, resultFile, imgFile, captureData, variablesData)
+	if err != nil {
+		return err
+	}
+	logType, logLevel := exec.RequiredLogs()
+	chromeCaps := chrome.Caps{}
+	caps := base.Capabilities{}
+	caps.SetLogLevel(logType, logLevel)
+	caps.SetChrome(chromeCaps)
+	driver := wd.NewWebDriver(caps, wdUrl, false)
+	if err := driver.Start(); err != nil {
+		return err
+	}
+	if err := exec.Start(driver); err != nil {
+		return err
+	}
+	return nil
 }
 
 func main() {
 	var showHelp bool
 	var showVersion bool
-	var execFile string
+	var sideFile string
 	var urlBase string
 	var port int
 	var urlPrefix string
 	var wdPath string
 	var wdOnly bool
-	var logFile string
+	var resultFile string
 	var imgFile string
 	var capture string
 	var variables string
 	flag.BoolVar(&showHelp, "h", false, "show this help")
 	flag.BoolVar(&showVersion, "v", false, "show version")
-	flag.StringVar(&execFile, "x", "", "exec file")
+	flag.StringVar(&sideFile, "x", "", "side file")
 	flag.StringVar(&urlBase, "b", "http://127.0.0.1", "web driver base url")
 	flag.StringVar(&urlPrefix, "u", "", "web driver prefix url")
 	flag.IntVar(&port, "p", 9515, "web driver port")
 	flag.StringVar(&wdPath, "s", "", "web driver service path")
 	flag.BoolVar(&wdOnly, "w", false, "start webdriver only")
-	flag.StringVar(&logFile, "l", defaultLogFile, "log file")
+	flag.StringVar(&resultFile, "l", defaultResultFile, "result file")
 	flag.StringVar(&imgFile, "i", defaultImagesFile, "images file")
 	flag.StringVar(&capture, "c", "", "capture event data (comma separated values)")
-	flag.StringVar(&variables, "z", "", "variables (es a=10;b=20")
+	flag.StringVar(&variables, "z", "", "side variables (es a=10;b=20), if you start the data with the letter @, the rest should be a filename (in ndjson format)")
 	flag.Parse()
 
 	if showHelp {
@@ -95,16 +126,19 @@ func main() {
 
 	wdUrl, err := url.Parse(sUrl)
 	if err != nil {
-		log.Fatal(err.Error())
+		fmt.Println(err.Error())
+		return
 	}
 
 	if len(wdPath) > 0 {
 		svc, err := service.NewChromeService(wdOnly, wdPath, wdUrl, true)
 		if err != nil {
-			log.Fatal(err.Error())
+			fmt.Println(err.Error())
+			return
 		}
 		if err := svc.Start(); err != nil {
-			log.Fatal(err.Error())
+			fmt.Println(err.Error())
+			return
 		}
 		if wdOnly {
 			fmt.Println("service webdriver successfully started")
@@ -112,38 +146,50 @@ func main() {
 		}
 	}
 
-	if len(execFile) == 0 {
-		fmt.Println("empty exec file")
+	if len(sideFile) == 0 {
+		fmt.Println("empty side file")
 		flag.Usage()
 		return
 	}
 
-	if len(logFile) == 0 {
-		logFile = defaultLogFile
+	if len(variables) == 0 {
+		if err := launch(wdUrl, sideFile, resultFile, imgFile, capture, nil); err != nil {
+			log.Fatal(err.Error())
+		}
+		return
 	}
 
-	if len(imgFile) == 0 {
-		imgFile = defaultImagesFile
+	if variables[0] != '@' {
+		variablesData := createVariables(variables)
+		if err := launch(wdUrl, sideFile, resultFile, imgFile, capture, variablesData); err != nil {
+			log.Fatal(err.Error())
+		}
+		return
 	}
 
-	exec, err := executor.New(execFile, logFile, imgFile, createCapture(capture), createVariables(variables))
+	variables = variables[1:]
+	file, err := os.OpenFile(variables, os.O_RDONLY, 0644)
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Println(err.Error())
+		return
 	}
-	logType, logLevel := exec.RequiredLogs()
-
-	chromeCaps := chrome.Caps{}
-	caps := base.Capabilities{}
-	caps.SetLogLevel(logType, logLevel)
-	caps.SetChrome(chromeCaps)
-	driver := wd.NewWebDriver(caps, wdUrl, false)
-
-	if err := driver.Start(); err != nil {
-		log.Fatal(err.Error())
+	scanner := bufio.NewScanner(file)
+	scanner.Buffer([]byte{}, bufio.MaxScanTokenSize*100)
+	counter := -1
+	for scanner.Scan() {
+		counter++
+		var variablesData map[string]interface{}
+		line := scanner.Text()
+		if err := json.Unmarshal([]byte(line), &variablesData); err != nil {
+			log.Printf("line %d: %s", counter, err.Error())
+			continue
+		}
+		if err := launch(wdUrl, sideFile, resultFile, imgFile, capture, variablesData); err != nil {
+			log.Printf("line %d: %s", counter, err.Error())
+		}
 	}
-
-	if err := exec.Start(driver); err != nil {
-		log.Fatal(err.Error())
+	if scanner.Err() != nil {
+		log.Printf("line %d: %s", counter, err.Error())
 	}
 
 	/*
