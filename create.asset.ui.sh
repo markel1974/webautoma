bfcd972fc49214e5aad9a484746207d03df4a524
markel1974 <markel@tin.it>
2024-05-23 16:37:07 +0200

    first

--- create.asset.ui.sh
+++ create.asset.ui.sh
@@ -1,1 +1,10 @@
+#!/bin/sh
+
+ROOT=$(pwd)
+cd ./src/server/web/ui/react || exit
+pwd
+#nvm use
+yarn run build
+sed -i'.backup' 's/<!--PRODUCTION_BUILD-->/<script src="build.js"><\/script>/' ./dist/index.html
+cd "${ROOT}" || exit
 ./build/webautoma -t web:./src/server/web/content:./src/server/web/ui/react/dist
