0847d1500fcf33c8c15654176371f468dad8b498
markel1974 <markel@tin.it>
2024-07-04 17:28:27 +0200

    first

--- src/version/version.go
+++ src/version/version.go
@@ -2,7 +2,7 @@ package version
 
 const MajorVersion = "1"
 const MinorVersion = "7"
-const PatchVersion = "9"
+const PatchVersion = "10"
 const AppVersionShort = MajorVersion + "." + MinorVersion + "." + PatchVersion + "." + BuildVersion
 const AppVersion = MajorVersion + "." + MinorVersion + "." + PatchVersion + "." + BuildVersion + " [" + BuildDate + "]"
 const AppName = "webautoma"
